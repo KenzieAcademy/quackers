aa7ad9fccb386fd564835044fca388bd283f420b
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-01-23 09:57:18 -0500

    test slack connection
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
 import os
 
-import slack
+import cherrypy
 import dotenv
+import slack
 
 dotenv.load_dotenv()
 
-# client = slack.WebClient(token=os.environ["BOT_USER_OAUTH_ACCESS_TOKEN"])
-#
-# client.chat_postMessage(
-#     channel="CSZPGCUAC",
-#     text="Hello from your app! :tada:",
-#     icon_emoji=":qbert:"
-# )
 
-@slack.RTMClient.run_on(event='message')
-def say_hello(**payload):
-    data = payload['data']
-    web_client = payload['web_client']
-    rtm_client = payload['rtm_client']
-    if 'Hello' in data.get('text', []):
-        channel_id = data['channel']
-        thread_ts = data['ts']
-        user = data['user']
+client = slack.WebClient(token=os.environ["BOT_USER_OAUTH_ACCESS_TOKEN"])
 
-        web_client.chat_postMessage(
-            channel=channel_id,
-            text=f"Hi <@{user}>!",
-            thread_ts=thread_ts
+
+class Root(object):
+    @cherrypy.expose
+    def question(self):
+        client.chat_postMessage(
+            channel="CSZPGCUAC",
+            text="Hello from your app! :tada:",
+            icon_emoji=":qbert:"
         )
+        return "thhhhtbbbbbt"
 
-slack_token = os.environ["BOT_USER_OAUTH_ACCESS_TOKEN"]
-rtm_client = slack.RTMClient(token=slack_token)
-rtm_client.start()
+
+cherrypy.config.update({'server.socket_port': 8090,
+                        'engine.autoreload.on': False,
+                        'log.access_file': './access.log',
+                        'log.error_file': './error.log'})
+cherrypy.quickstart(Root())
+
