106c268fc2ad716837b3fd5fd702d026f5741c53
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-01-23 12:27:40 -0500

    modal test
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- main.py
+++ main.py
@@ -6,21 +6,62 @@ from flask import Flask, request, jsonify
 
 dotenv.load_dotenv()
 
-
 client = slack.WebClient(token=os.environ["BOT_USER_OAUTH_ACCESS_TOKEN"])
 
 app = Flask(__name__)
 
+modal = {
+    "type": "modal",
+    "title": {
+        "type": "plain_text",
+        "text": "QBert!",
+        "emoji": True
+    },
+    "submit": {
+        "type": "plain_text",
+        "text": "Submit",
+        "emoji": True
+    },
+    "close": {
+        "type": "plain_text",
+        "text": "Cancel",
+        "emoji": True
+    },
+    "blocks": [
+        {
+            "type": "input",
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "ml_input",
+                "multiline": True
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "What else should we know about the problem you're facing?"
+            },
+            "hint": {
+                "type": "plain_text",
+                "text": "Any context you can provide will help!"
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "mrkdwn",
+                    "text": "*NOTE*: Your question won't get sent to the coaches until you click submit!"
+                }
+            ]
+        }
+    ]
+}
+
+
 @app.route('/question/', methods=['POST'])
 def question():
-    data = request.json
+    data = request.form.to_dict(flat=False)
 
-    client.chat_postMessage(
-        channel="CSZPGCUAC",
-        text="Hello from your app! :tada:",
-        icon_emoji=":qbert:"
-    )
-
-    return jsonify(data)
-
-
+    return jsonify(modal)
