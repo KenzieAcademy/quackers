0efc356be6734214d3c8a3bcbb9c0deb0676db40
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-01-23 11:08:52 -0500

    flask time, I guess

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
 import os
 
-import cherrypy
 import dotenv
 import slack
+from flask import Flask, request, jsonify
 
 dotenv.load_dotenv()
 
 
 client = slack.WebClient(token=os.environ["BOT_USER_OAUTH_ACCESS_TOKEN"])
 
+app = Flask(__name__)
 
-class Root(object):
-    @cherrypy.expose
-    def question(self):
-        client.chat_postMessage(
-            channel="CSZPGCUAC",
-            text="Hello from your app! :tada:",
-            icon_emoji=":qbert:"
-        )
-        return "thhhhtbbbbbt"
+@app.route('/question/', methods=['POST'])
+def question():
+    data = request.json
 
+    client.chat_postMessage(
+        channel="CSZPGCUAC",
+        text="Hello from your app! :tada:",
+        icon_emoji=":qbert:"
+    )
 
-cherrypy.config.update(
-    {
-        'server.socket_port': 8000,
-        'server.socket_host': '0.0.0.0',
-        'engine.autoreload.on': False,
-        'log.access_file': './access.log',
-        'log.error_file': './error.log'
-    }
-)
-cherrypy.quickstart(Root())
+    return jsonify(data)
+
 
